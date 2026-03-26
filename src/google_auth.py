0b7d21567709459f5627707c4e828b67a83508eb
Ben Hailwood <bhtest260326@gmail.com>
2026-03-27 00:01:39 +0800

    fix circular import

---
+++ src/google_auth.py
@@ -0,0 +1,31 @@
+import os
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/gmail.send',
+    'https://www.googleapis.com/auth/calendar'
+]
+
+def get_gmail_service():
+    creds = Credentials(
+        token=None,
+        refresh_token=os.environ['GOOGLE_REFRESH_TOKEN'],
+        client_id=os.environ['GOOGLE_CLIENT_ID'],
+        client_secret=os.environ['GOOGLE_CLIENT_SECRET'],
+        token_uri='https://oauth2.googleapis.com/token',
+        scopes=SCOPES
+    )
+    return build('gmail', 'v1', credentials=creds)
+
+def get_calendar_service():
+    creds = Credentials(
+        token=None,
+        refresh_token=os.environ['GOOGLE_REFRESH_TOKEN'],
+        client_id=os.environ['GOOGLE_CLIENT_ID'],
+        client_secret=os.environ['GOOGLE_CLIENT_SECRET'],
+        token_uri='https://oauth2.googleapis.com/token',
+        scopes=SCOPES
+    )
+    return build('calendar', 'v3', credentials=creds)
